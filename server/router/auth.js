ab862301316fbee0ffb3ec44cb104fa49a118f3d
chan-0912 <yadavchanchal679@gmail.com>
2022-11-06 15:57:01 +0530

    return

--- server/router/auth.js
+++ server/router/auth.js
@@ -36,6 +36,7 @@ router.post("/register", async (req, res) => {
 
       await user.save();
       res.status(201).json({ message: "user registered successfully" });
+      return;
     }
   } catch (err) {
     console.log(err);
